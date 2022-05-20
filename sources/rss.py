e1d1b75401c47d566441a9d36fb0ee54a643d08c
iamsk <iamsk.info@gmail.com>
2022-05-20 10:35:48 +0800

    html to text

--- sources/rss.py
+++ sources/rss.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import requests
 import feedparser
 from benedict import benedict
+from inscriptis import get_text
 
 
 def _shorten(text, width):
@@ -21,11 +22,13 @@ class RSS(object):
             resp = requests.get(self.rss_url, timeout=10, verify=False)
             ret = feedparser.parse(resp.text)
         except Exception as e:
+            print(e)
             return []
         records = []
-        for entry in ret.entries[:3]:  # limit to new articles within yesterday
+        for entry in ret.entries[:20]:  # limit to new articles within yesterday
             try:
-                dt = datetime.fromtimestamp(mktime(entry.published_parsed))
+                dt = datetime.fromtimestamp(
+                    mktime(entry.published_parsed if hasattr(entry, 'published_parsed') else entry.updated_parsed))
             except Exception as e:
                 # some feeds missed published_parsed, example, https://www.insightpartners.com/blog/rss/
                 continue
@@ -33,7 +36,11 @@ class RSS(object):
                 continue
             keys = ['title', 'link', 'published', 'summary', 'author', 'published_parsed', 'tags']
             record = benedict(entry).subset(keys=keys)
-            record['summary'] = _shorten(record['summary'], 200)
+            text = get_text(record['summary'])
+            text = text.replace(record['title'], '')
+            text = text.replace('\n', '')
+            record['summary'] = _shorten(text, 100).strip()
+            # import pdb;pdb.set_trace()
             # record['tags'] = [tag['term'] for tag in entry.tags]
             records.append(record)
         return records
